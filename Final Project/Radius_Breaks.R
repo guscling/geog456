596a3d6a440b65150551457d4a5d24c50b2e2cc0
guscling <guscling@email.unc.edu>
2024-04-22 10:16:16 -0400

    final

---
+++ Final Project/Radius_Breaks.R
@@ -0,0 +1,30 @@
+library(classInt)
+rm(list=ls(all=TRUE))
+setwd('/Users/gusclingroth/GEOG456/Final Project/Csv')
+migration_data <- read.csv('GEOG456_Total_Data.csv')
+
+a <- min(migration_data$Assylum_Apps_2014)
+b <- min(migration_data$Assylum_Apps_2015)
+c <- min(migration_data$Assylum_Apps_2016)
+d <- min(migration_data$Assylum_Apps_2017)
+e <- min(migration_data$Assylum_Apps_2018)
+f <- min(migration_data$Assylum_Apps_2019)
+g <- min(migration_data$Assylum_Apps_2020,na.rm=T)
+h <- min(migration_data$Assylum_Apps_2021,na.rm=T)
+i <- min(migration_data$Assylum_Apps_2022,na.rm=T)
+j <- min(migration_data$Assylum_Apps_2023,na.rm=T)
+
+mins <- c(a,b,c,d,e,f,g,h,i,j)
+min(mins)
+#1216869
+
+total_migration <- migration_data[,c(1,34:43)]
+total_migration_no_country <- total_migration[,-c(1)]
+total_migration_array <- array(unlist(total_migration_no_country))
+total_migration_array <- na.omit(total_migration_array)
+sorted_array <- sort(total_migration_array)
+
+classify_intervals(var=sorted_array,n=7,style="kmeans")
+
+
+
